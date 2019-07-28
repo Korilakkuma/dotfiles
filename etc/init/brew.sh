e26b50db7f2f216f00c56d1b2cf6d286365c82f3
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2019-07-28 20:17:36 +0900

    Fix brew.sh

--- etc/init/brew.sh
+++ etc/init/brew.sh
@@ -4,28 +4,29 @@ brew update
 brew upgrade
 
 brew install docker
-brew install ffmpeg --with-libvpx
+brew install ffmpeg
 brew install git
 brew install go
 brew install hugo
-brew install imagemagick --with-webp
+brew install imagemagick
 brew install jq
 brew install tmux
 brew install vim
 brew install wget
 brew install zsh
 
-brew cast install adobe-creative-cloud
-brew cast install firefox
+# brew cask install adobe-creative-cloud
+brew cask install docker
+brew cask install firefox
 brew cask install google-chrome
-brew cask install google-chrome-canary
-brew cast install iterm2
+# brew cask install google-chrome-canary
+brew cask install iterm2
 brew cask install libreoffice
-brew cast install opera
-brew cast install sketch
+# brew cask install opera
+brew cask install sketch
 brew cask install slack
 brew cask install vlc
 
 curl -L git.io/nodebrew | perl - setup
 
-brew cleanup
+# brew cleanup
